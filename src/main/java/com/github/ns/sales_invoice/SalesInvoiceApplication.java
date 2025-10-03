1f84d806265d40d137cfc7ad1827741b7abb1e1b
cleytonorocha <cleytoodeoliveirarocha@gmail.com>
2025-10-03 19:31:43 -0300

    first commit

---
+++ src/main/java/com/github/ns/sales_invoice/SalesInvoiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.ns.sales_invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesInvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesInvoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ns/sales_invoice/SalesInvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ns.sales_invoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesInvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
